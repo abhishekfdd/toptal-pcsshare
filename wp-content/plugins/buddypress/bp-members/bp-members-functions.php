f347091c631147e5155709dc6b089cebd8ad28f7
Tin Chu <tinho@toptal.com>
2018-07-13 09:35:36 -0500

    changes errors validators

--- wp-content/plugins/buddypress/bp-members/bp-members-functions.php
+++ wp-content/plugins/buddypress/bp-members/bp-members-functions.php
@@ -1582,7 +1582,7 @@ function bp_core_validate_user_signup( $user_name, $user_email ) {
 
 		// User name must pass WP's validity check.
 		if ( ! validate_username( $user_name ) ) {
-			$errors->add( 'user_name', __( 'Usernames can contain only letters, numbers, ., -, and @', 'buddypress' ) );
+			$errors->add( 'user_name', __( 'Usernames can contain only letters, numbers, and -', 'buddypress' ) );
 		}
 
 		// Minimum of 4 characters.
